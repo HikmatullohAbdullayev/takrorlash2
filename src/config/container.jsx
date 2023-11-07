3f7be46f34eb1c08eb0ed3e2edf3b7353e7d4a27
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-11-07 12:07:45 +0500

    global style done

---
+++ src/config/container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+ export const Container =styled.div`
+    margin: 0 auto;
+    padding: 0 20px;
+    max-width: 1340px;
+    scroll-behavior: smooth;
+    box-sizing: border-box;
+
+`
