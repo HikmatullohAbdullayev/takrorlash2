ea2928745c7eb3bb37a661260e50a33d91dba1d5
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-11-15 12:12:39 +0500

    header HTML done

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,9 +1,68 @@
 import React from "react";
-export const  Header = () =>{
-    return(
-        <>
-        <h2>header</h2>
-        </>
-    )
-    
-}
+import { Link } from "react-router-dom/dist";
+import logoIgm from "../../assets/img/logo.png";
+export const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <a className="header__logo-link" href="#">
+          <img className="header__logo" src={logoIgm} alt="logo" />
+        </a>
+        <nav className="header__nav nav">
+          <ul className="nav__list list">
+            <li className="list__item">
+              <Link className="item__link" to={"home"}>
+                Home
+              </Link>
+            </li>
+            <li className="list__item">
+              <Link className="item__link" to={"shop"}>
+                Shop
+              </Link>
+            </li>
+            <li className="list__item">
+              <Link className="item__link" to={"product"}>
+                Product
+              </Link>
+            </li>
+            <li className="list__item">
+              <Link className="item__link" to={"page"}>
+                Page
+              </Link>
+            </li>
+          </ul>
+        </nav>
+       
+
+        <ul className="icon-box">
+          <li className="icon-item"></li>
+          <li className="icon-item">
+            {" "}
+            <span className="header_icon"> icon </span>{" "}
+          </li>
+          <li className="icon-item">
+            {" "}
+            <Link className="item-link" to={"profile"}>
+              {" "}
+              <span className="header_icon"> icon </span>{" "}
+            </Link>
+          </li>
+          <li className="icon-item">
+            {" "}
+            <Link className="item-link" to={"selected"}>
+              {" "}
+              <span className="header_icon"> icon </span>{" "}
+            </Link>
+          </li>
+          <li className="icon-item">
+            {" "}
+            <Link className="item-link" to={"korzinka"}>
+              {" "}
+              <span className="header_icon"> icon  <sup className="shop_count">0 </sup> </span>{" "}
+            </Link>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+};
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom/dist";
 import "./homeStyle.module.css";
+import "../../config/global.css"
 import React from "react";
+import { Header } from "../../layout/header/header";
 
 export const Home = () => {
   return (
     <>
+    <div className="container">
+
+        <Header/>
     
-      <Link to="Shop">salom </Link>
-      <Link to="product">product </Link>
-      <Link to="page">page </Link>
-      <Link to="profile">profile </Link>
-      <Link to="Shop">icon </Link>
-      <Link to="Shop">icon 2 </Link>
-      <Link to="Shop">salom </Link>
+      
+    </div>
     </>
   );
 }; 
