dad1f244a9b18917dce050276c628c562d313d66
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-11-16 23:37:34 +0500

    commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom/dist";
-import "./homeStyle.module.css";
 import "../../config/global.css"
 import React from "react";
 import { Header } from "../../layout/header/header";
